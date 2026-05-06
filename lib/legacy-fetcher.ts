b325057b99996640a22d7d14ac7c33ee70ec0c35
yashgoyal0110 <yg364550@gmail.com>
2026-05-06 23:31:50 +0530

    clean out legacy-fetcher

--- lib/legacy-fetcher.ts
+++
@@ -1,6 +0,0 @@
-// legacy fetch wrapper, kept until the new client lands
-export async function legacyFetch(url: string) {
-  const res = await fetch(url);
-  if (!res.ok) throw new Error("request failed");
-  return res.json();
-}
