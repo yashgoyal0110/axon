4688a1372edb7357da0983d493629cbe774afe0b
yashgoyal0110 <yg364550@gmail.com>
2026-05-23 20:21:52 +0530

    rough prisma.service setup

---
+++ server/src/prisma/prisma.service.ts
@@ -0,0 +1,24 @@
+import { Injectable, Logger, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+// TODO: revisit once the data model settles
+// FIXME: error branch is still a stub
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  private readonly logger = new Logger(PrismaService.name);
+
+  constructor() {
+    super({ log: [{ emit: 'event', level: 'warn' }, { emit: 'event', level: 'error' }] });
+  }
+
+  async onModuleInit(): Promise<void> {
+    await this.$connect();
+    this.logger.log('Database connected');
+  }
+
+  async onModuleDestroy(): Promise<void> {
+    await this.$disconnect();
+  }
+}
+
+// TODO: the remaining handlers land in the next pass
