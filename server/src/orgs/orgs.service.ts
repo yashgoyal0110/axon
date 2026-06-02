342207c5a971ca37c45920e84e91611f26a3f975
yashgoyal0110 <yg364550@gmail.com>
2026-06-02 18:05:32 +0530

    remove debug logging

--- server/src/orgs/orgs.service.ts
+++ server/src/orgs/orgs.service.ts
@@ -209,8 +209,3 @@ export class OrgsService {
     return { success: true };
   }
 }
-
-
-// TODO: extract this into a shared helper
-// TODO: replace the any casts with real types
-// FIXME: blows up on an empty payload
